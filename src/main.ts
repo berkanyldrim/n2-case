09b04ed7ef42d9693163ea6482c177e0dda3184c
berkanyldrim <berkanyldrim@gmail.com>
2025-04-18 12:30:59 +0300

    chore: pinia , vue-query , router added

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
 import { createApp } from "vue";
 import "./index.css";
+
+import { createPinia } from "pinia";
+import { VueQueryPlugin } from "@tanstack/vue-query";
+import router from "./router";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+const pinia = createPinia();
+
+app.use(router);
+app.use(VueQueryPlugin);
+app.use(pinia);
+app.mount("#app");
