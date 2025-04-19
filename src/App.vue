8b4c12621a8f7428c707ad03534e3453e97f8cae
berkanyldrim <berkanyldrim@gmail.com>
2025-04-19 16:05:07 +0300

    chore: user data load added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
 import { SidebarProvider } from "./components/ui/sidebar";
 import AppLayout from "./components/AppLayout.vue";
+import { onMounted } from "vue";
+import { useUserStore } from "@/store/userStore";
+
+const userStore = useUserStore();
+
+onMounted(() => {
+  userStore.loadUser();
+});
 </script>
 
 <template>
