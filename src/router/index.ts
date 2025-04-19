b3a4b41067ab6261cba118c8b0d87add7e6e52f1
berkanyldrim <berkanyldrim@gmail.com>
2025-04-19 16:07:14 +0300

    chore: router completed

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,9 @@
 import { createWebHistory, createRouter } from "vue-router";
-import Home from "../Home.vue";
-import TodoView from "../views/TodoView.vue";
+import Home from "@/Home.vue";
+import TodoView from "@/views/TodoView.vue";
+import PostView from "@/views/PostView.vue";
+import AlbumView from "@/views/AlbumView.vue";
+import AlbumDetailView from "@/views/AlbumDetailView.vue";
 
 const routes = [
   {
@@ -12,6 +15,21 @@ const routes = [
     path: "/user/:userId/todos",
     component: TodoView,
     name: "todos",
+  },
+  {
+    path: "/user/:userId/posts",
+    component: PostView,
+    name: "posts",
+  },
+  {
+    path: "/user/:userId/albums",
+    component: AlbumView,
+    name: "albums",
+  },
+  {
+    path: "/user/:userId/albums/:albumId",
+    component: AlbumDetailView,
+    name: "album-detail",
   },
 ];
 
